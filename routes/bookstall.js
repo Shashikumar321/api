6d01a052ad2231206ef718de7169f4f97c5b8463
shashikumar321 <shashikumaryn3@gmail.com>
2022-03-03 12:32:43 +0530

    Creatd API's

--- routes/bookstall.js
+++ routes/bookstall.js
@@ -1,11 +1,19 @@
 const bookstall = require('express').Router();
+const booksController = require('../controller/bookstallController');
 
 bookstall.get('/', (request, response) => {
     response.send("Welcome to UI.Focus");
 })
 
+//heartbeat route
 bookstall.get('/heartbeat', (request, response) => {
     response.send("Application is LIVE!");
 })
+
+//Fetch all books
+bookstall.get('/books', booksController.getAllBooks);
+
+//Fetch a single book
+bookstall.get('/books/:id', booksController.getBookById);
 
 module.exports = bookstall;
